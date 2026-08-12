70512a6a80ae405f670a66a1420168f93dcfc281
The-Rizzler-V2 <freddyfatbear.poop@gmail.com>
2026-08-12 16:12:21 -0400

    Add files via upload

---
+++ Fe.lua
@@ -0,0 +1,15 @@
+if workspace:FindFirstChild("FEBYPASSLOAD") then
+  warn("How would you already even have it loaded?")
+  return
+else
+end
+
+local Version = 69.420
+
+local settings = {
+  idiot = true;
+}
+
+Instance.new("Folder", game:GetService("Workspace")).Name = "FEBYPASSLOAD"
+
+game.Players.LocalPlayer:Kick("You're Retarded")
