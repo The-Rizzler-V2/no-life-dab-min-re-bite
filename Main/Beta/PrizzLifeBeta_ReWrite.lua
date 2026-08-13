415d547a1259d30bf5f4686c04bb732a4f226610
The-Rizzler-V2 <freddyfatbear.poop@gmail.com>
2026-08-12 23:34:37 -0400

    Update PrizzLifeBeta_ReWrite.lua

--- Main/Beta/PrizzLifeBeta_ReWrite.lua
+++ Main/Beta/PrizzLifeBeta_ReWrite.lua
@@ -1093,7 +1093,7 @@ local function tpbypass(root,pos)
 	local connection
 	connection = Services.rsv.RenderStepped:Connect(function()
 		local alpha = math.clamp((tick() - starttime) / dur, 0, 1)
-		alpha = 1 - (1 - alpha) ^ 0.75
+		alpha = 1 - (1 - alpha) ^ 3.5
 
 		root.CFrame = startpos:Lerp(pos, alpha)
 
