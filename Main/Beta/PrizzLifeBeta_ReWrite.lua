338be2554e1f269e8f1d130c32848519feef5c4a
The-Rizzler-V2 <freddyfatbear.poop@gmail.com>
2026-08-12 23:40:14 -0400

    Update PrizzLifeBeta_ReWrite.lua

--- Main/Beta/PrizzLifeBeta_ReWrite.lua
+++ Main/Beta/PrizzLifeBeta_ReWrite.lua
@@ -1086,7 +1086,7 @@ end
 local function tpbypass(root,pos)
 	local startpos = root.CFrame
 	local dist = (pos.Position - startpos.Position).Magnitude
-	local dur = dist / 0.5
+	local dur = dist / 2
 	local starttime = tick()
 	local isdone = false
 
